835bdca5ca39dc8dc33e6d56a189a21bf17aaade
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-08-29 14:32:56 +0500

    Assignment 14 to 16 added

--- array.js
+++
@@ -1,9 +0,0 @@
-var array = [];
-var econdArray = [];
-var stringArray = ['Mughis'];
-var numberArray = [1,2,3,4,5];
-var boolArray = [true, false];
-var mixedArray = [1,2,'String',true];
-var qualificationInPakistan = ['SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS', 'M.Phil', 'PhD'];
-console.log(qualificationInPakistan);
-
